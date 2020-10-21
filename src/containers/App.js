6cac24d66df71416364a0d4d26ae66dbc36594ab
yasnaabyat <63559743+yasnaabyat@users.noreply.github.com>
2020-10-21 21:32:44 +0300

    changing

--- src/containers/App.js
+++ src/containers/App.js
@@ -34,7 +34,7 @@ class App extends Component  {
             return robot.name.toLowerCase().includes(SearchField.toLowerCase())
         })
         return !robots.length ?
-            <h1>Loading</h1> :
+            <h1 className='tc'>Loading...</h1> :
         (
             <div className='tc'>
             <h1 className='f2'>Robo Friends</h1>
